71150bb7f76102232d5f5a1f991c8899bb757d38
Jan Buschtöns <jan@buschtoens.me>
2021-12-02 23:31:53 +0100

    chore(eslint): silence `any`

--- globals.ts
+++ globals.ts
@@ -6,4 +6,5 @@ import { getGlobals } from './get-globals';
  * @remarks Intentionally not exporting `requirejs` & `requireModule` as they
  * are just aliases of `require` and would cause confusion.
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const { loader, define, require } = getGlobals(window as any);
